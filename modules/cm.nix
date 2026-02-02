1c2def48336e6e487fcb9c6db4e5c9c14cfc5174
Arthur Wallendorff <howdy@wallenart.dev>
2026-02-03 00:44:19 +0100

    chore: format

--- modules/cm.nix
+++ modules/cm.nix
@@ -11,14 +11,13 @@
   ...
 }:
 {
-  boot.kernelParams =
-    [
-      "8250.nr_uarts=1" # Number of 8250 UARTs to register
-      "console=tty1"
-    ]
-    # BCM2835 audio driver settings (CM4 only)
-    ++ lib.optionals isCM4 [
-      "snd_bcm2835.enable_hdmi=1"
-      "snd_bcm2835.enable_headphones=1"
-    ];
+  boot.kernelParams = [
+    "8250.nr_uarts=1" # Number of 8250 UARTs to register
+    "console=tty1"
+  ]
+  # BCM2835 audio driver settings (CM4 only)
+  ++ lib.optionals isCM4 [
+    "snd_bcm2835.enable_hdmi=1"
+    "snd_bcm2835.enable_headphones=1"
+  ];
 }
